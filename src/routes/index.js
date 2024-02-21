67587a5bfa3ece1199ffc2bad078f316686a6931
Nicolas Buszczak <nico_947quil@hotmail.com>
2024-02-20 21:28:46 -0300

    Server working with test routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
 const { Router } = require('express');
+const path = require('path');
+
 const router = Router();
 
-router.get('/data.cgi', (req,res)=>{res.status(200).json({x:160,y:Math.floor(Math.random() * (30 - 10 + 1)) + 10})});
-router.get('/page', (req,res)=>{res.sendFile("C:/Users/nico_/Dropbox/Proyecto final - Carga electronica/4. Codigos/Interfaz Web/graph.html")});
-router.get('/chart.js', (req,res)=>{res.sendFile("C:/chart.js")});
+router.get('/data.cgi', (req, res) => {
+    res.status(200).json({ x: 160, y: Math.floor(Math.random() * (30 - 10 + 1)) + 10 });
+});
+
+router.get('/page', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'webpage', 'graph.html'));
+});
+
+router.get('/chart.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'library', 'chart.js'));
+});
 
 module.exports = router;
